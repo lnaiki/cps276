e16049c0a8ac606675921c7e635098cde60f8c7b
Leona Naiki Chase <leona.naiki@gmail.com>
2021-09-22 22:39:45 -0400

    Updates

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -44,20 +44,23 @@
                 <input type="text" class="form-control" id="inputZip">
               </div>
 
-<!--QUESTION HERE ABOUT DOING AN IN-LINE RADIO -->
-              <div class=".form-check .form-check-inline">
-                <input class="form-check-input" type="radio" name="genderOptions" id="male" value="option1">
-                <label class="form-check-label" for="male">Male</label>
-              </div>
-              <div class=".form-check .form-check-inline">
-                <input class="form-check-input" type="radio" name="genderOptions" id="female" value="option2">
-                <label class="form-check-label" for="female">Female</label>
+              <div class="col-2">            
+                <div class="row">
+                  <div class="col">
+                    <input class="form-check-input" type="radio" name="genderOptions" id="male" value="option1">
+                    <label class="form-check-label" for="male">Male</label>
+                  </div>
+                  <div class="col">
+                    <input class="form-check-input" type="radio" name="genderOptions" id="female" value="option2">
+                    <label class="form-check-label" for="female">Female</label>
+                  </div>
+                </div>
               </div>
 
 
-              <div class="col-12">
-                <button type="submit" class="btn btn-primary">Register</button>
-              </div>
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary">Register</button>
+            </div>
 
           </form>
           
