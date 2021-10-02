0d7276dd61b49844d2457817495a80d295fc4764
Leona Naiki Chase <leona.naiki@gmail.com>
2021-10-02 16:25:55 -0400

    Updates

--- Assignments/assignment2/exercise1.php
+++ Assignments/assignment2/exercise1.php
@@ -1,9 +1,12 @@
 <?php      
-           
+         
+    $x = 4;
+    $y = 5;
+        
     $output = "";
-    for ($i = 1; $i < 5; $i++){
+    for ($i = 1; $i <= $x; $i++){
         $output.= '<li>'.$i . '</li> <ul>' ;
-            for ($j = 1; $j < 6; $j++){
+            for ($j = 1; $j <= $y; $j++){
             $output .= '<li>' . $j . '</li>';
             }
     $output .= '</ul>';
@@ -18,7 +21,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Assignment 2.1</title>
 </head>
 <body>
 
