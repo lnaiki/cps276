92dbef86d598a568ee00056f0f17f4dd795245e7
Leona Naiki Chase <leona.naiki@gmail.com>
2021-11-20 11:54:03 -0500

    updates

--- Assignments/assignment9/index.php
+++ Assignments/assignment9/index.php
@@ -20,7 +20,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>Form</title>
+    <title>Add Notes</title>
   </head>
   <body>
     <main class="container">
